219a790bd13b73844a1b4a86bf93b8f454c53412
Stephanie Lauziere <salauzie@gmail.com>
2015-04-29 17:04:37 -0400

    started blog page

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,35 @@
+class BlogsController < ApplicationController
+
+	def index
+		@blogs = Blog.all
+	end
+
+	def new
+		@blog = Blog.new
+	end
+
+	def show
+		@blog = blog.find params[:id]
+	end
+	
+	def edit
+		@blog = Blog.find params [:id]
+	end	
+
+	def update
+		@blog = Blog.find params[:id]
+		@blog.update_attributes blog_params
+		redirect_to blogs_path
+	end
+	
+	def create
+		@blog = Blog.create blog_params
+		redirect_to blogs_path
+	end	
+
+	def destroy
+		@blog = Blog.find params[:id]
+		@blog.delete
+		redirect_to blogs_path
+	end	
+end
